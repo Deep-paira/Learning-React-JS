0ec11c42d153a6b619afd9a0acfa488b9d1b5532
Deep paira <deeppaira45@gmail.com>
2025-08-30 23:39:16 +0530

    first commit

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>DELTA | running this code with vite. And delete the logo, css files because we don't need them, and keep the src file clean.</h1>
+  )
+}
+
+export default App
