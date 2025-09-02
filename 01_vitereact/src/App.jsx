1d48a9245c5735a2ae92ea76e4180e265d98b3d9
Deep paira <deeppaira45@gmail.com>
2025-09-02 16:35:30 +0530

    4th commit

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,6 +1,5 @@
 import Delta from "./Delta"
 
-
 function App() {
 
   return (
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,9 +1,42 @@
+
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Hello from myapp !</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = "Code with delta"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    anotherElement
+)
+
+
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    <MyApp />
   
 )
